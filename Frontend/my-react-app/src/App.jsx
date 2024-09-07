06c848f3641137f782f700dcb6bb554c4f5de5a9
joshuaconrad04 <140972248+joshuaconrad04@users.noreply.github.com>
2024-09-07 14:00:26 -0500

    first commit

---
+++ Frontend/my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Slider } from '@mui/base/Slider';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Hi welcome to my Portfolio Website
+        <Slider></Slider>
+      </div>
+    </>
+  )
+}
+
+export default App
