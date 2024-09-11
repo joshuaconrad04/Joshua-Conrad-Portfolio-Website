d45fa0515614de26b6ecfe44f8cc97e0d910c66f
joshuaconrad04 <140972248+joshuaconrad04@users.noreply.github.com>
2024-09-11 00:12:00 -0500

    Css added to homepage HTML

--- Frontend/my-react-app/src/components/Home.jsx
+++ Frontend/my-react-app/src/components/Home.jsx
@@ -4,36 +4,15 @@ import styles from "../styles/Home.module.css";
 function Home(){
   return (
 
-  <div class="flex-container">
-<h1 >Hi welcome to my Portfolio Website</h1>
+    <div className={styles.container}>
+    <h1 className={styles.header}>Hi welcome to my Portfolio Website</h1>
+    <h2>Computer Science Major & Math-Minor</h2>
+    <p>Specializing in FullStack Development w/ React for the front-end and Node JS as well as Springboot for the backend</p>
+    <img className={styles.picture} src="/Joshua_Head_Shot.jpg" alt="Picture of Me" />
+    <a href="https://github.com/joshuaconrad04">GitHub</a>
+    <a href="https://www.linkedin.com/in/joshua-conrad-3a7725219/">LinkedIn</a>
 
-
-
-<h2>Computer Science Major & Math-Minor</h2>
-<br></br>
-<p>Specializing in FullStack Development w/ React for the front-end and Node JS as well as Springboot for the backend</p>
-<img src="/Joshua_Head_Shot.jpg" alt="Picture of Me" />
-<br></br>
-<a href="https://github.com/joshuaconrad04">GitHub</a>
-<br></br>
-<a href="https://www.linkedin.com/in/joshua-conrad-3a7725219/">LinkedIn</a>
-<br></br>
-<h3>Technologies and Skills</h3>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <h3>Technologies and Skills</h3>
 
   </div>
   )
